8a4ae185a7afc9c0002f583f1053ee3ee336365a
Eugene <90315914+Ohhcherry@users.noreply.github.com>
2023-01-09 22:18:31 +0300

    Коррекция по комментариям

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -38,8 +38,8 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO users(name, lastname, age) VALUES (?,?,?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+//Сохранение параметров пользователей в таблицу
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(name, lastname, age) VALUES (?,?,?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
